fda00c7152ef94a29c3c8ccbd95b8f892a0ad71b
aanchallulla <aanchallulla15@gmail.com>
2025-08-14 16:05:13 +0200

    Wide dataset has been created

--- Edudata.R
+++ Edudata.R
@@ -5,53 +5,156 @@ library(ggplot2)  # Visualization
 library(tidyr)    # Data tidying
 library(readxl)
 
-##Loading the dataset 
+##Loading the dataset
 terridata<- read_excel("Thesis /Education dataset.xlsx")
-View(terridata)                                                                                      
-glimpse(terridata)
 
-##Trying to understand the dataset 
-head(terridata)
+##Exploring the dataset
+View(terridata)
+glimpse(terridata)
 str(terridata)
 unique(terridata$`Entity Code`)
-unique(terridata$`Indicator`)
-##Cleaning the dataset 
+length(unique(terridata$Entity))
+length(unique(terridata$Department))
+length(unique(terridata$Department))
+length(unique(terridata$`Indicator`))
+length(unique(terridata$`Numerical Data`))
+is.na(terridata$`Numerical Data`)
+length(is.na(terridata$`Numerical Data`))
+sum(is.na(terridata$`Numerical Data`))
+
+terridata %>%
+  filter(`Indicator` == "Average score on the Saber 11 test - Critical reading"|
+           `Indicator` =="Average score on Saber 11 tests - Mathematics") %>%
+  arrange(`Indicator`) %>%
+  View
+
+
+terridata %>%
+  filter((`Indicator` == "Gross coverage in transition") &
+           (`Department` == "Bolívar")) %>%
+  View
+
+
+glimpse(terridata$`Numerical Data`)
+unique(terridata$`Numerical Data`)
+
+##Modifying the dataset
+terridata <- terridata %>%
+mutate(
+`Numerical Data` = str_replace_all(`Numerical Data`, ",", "."),
+)
+
+terridata %>%
+filter(`Numerical Data` == "0") %>%
+View() ## we have a total of 7,388 obs
+
+#checking the number of NA
+terridata %>%
+filter(is.na(`Numerical Data`)) %>%
+View() ## we have a total of 23,673 obs
+
+length(unique(terridata$`Numerical Data`))
+sample(unique(terridata$`Numerical Data`), 50)
+glimpse(terridata$`Numerical Data`)
+unique(terridata$`Numerical Data`)
+head(sort(unique(terridata$`Numerical Data`)), 50)
+tail(sort(unique(terridata$`Numerical Data`)), 50)
+head(sort(unique(terridata$`Numerical Data`)), 100)
+tail(sort(unique(terridata$`Numerical Data`)), 100)
+summary(as.numeric(terridata$`Numerical Data`))
+terridata$`Numerical Data`[is.na(terridata$`Numerical Data`)]
+terridata$`Numerical Data`[is.na(terridata$`Numerical Data`)]
+
+##Converting chr to numeric
 
 terridata <- terridata %>%
-  mutate(
-    `Numerical Data` = str_replace(`Numerical Data`, ",", "."),
-    `Numerical Data` = as.numeric(`Numerical Data`),
-    Year = as.numeric(Year)
-  )
+mutate(
+`Numerical Data` = as.numeric(`Numerical Data`)
+)           ##This introduces NAs by coercion and thus we now have 25,797 obs - 2124 more.
 
-summary(terridata$`Numerical Data`)  # Check min/max/NA count
-table(terridata$Year)                # Check year range
+terridata <- terridata %>%
+mutate(
+Year = as.numeric(Year)
+)
 
-##Remove unnecesary columns
+summary(as.numeric(terridata$`Numerical Data`))
+glimpse(terridata)
+
+##Removing unnecssary columns
 filtered_data <- terridata%>%
-  select(
-    -Mes,                   # Remove month column
-    -`Qualitative Data`,     # Remove empty qualitative column
-    -Source                 # Remove source column (optional)
-  )
-
-##
-
-wide_data <- filtered_data %>%
-  pivot_wider(
-    # Columns that identify unique rows (will stay as-is)
-    id_cols = c(`Department Code`, Department, `Entity Code`, Entity, Year),
-    
-    # Column whose values will become new column names
-    names_from = Indicator,
-    
-    # Columns whose values will fill the new columns
-    values_from = c(`Numerical Data`, `Unit of Measurement`),
-    
-    # Format new column names as "Indicator_[Numerical Data/Unit of Measurement]"
-    names_glue = "{Indicator}_{.value}"
-  )
+select(
+-Mes,                   # Remove month column
+-`Qualitative Data`,     # Remove empty qualitative column
+-Source                 # Remove source column
+)
+filtered_data %>% distinct() 
+filtered_data[!duplicated(filtered_data),]
+filtered_data[duplicated(filtered_data),] ##This tells us that we have 1655 row duplicates in the filtered data
 
 
+##Do we need this code? It gives us the Indicators that are duplicated and we also don't need.
+duplicates <- filtered_data |>
+dplyr::summarise(n = dplyr::n(),
+.by = c(`Department Code`, Department, `Entity Code`, Entity,
+Dimension, Subcategory, Year, `Unit of Measurement`, Indicator)) |>
+dplyr::filter(n > 1L)
+View(duplicates)
+unique(duplicates$Indicator)
 
+##This code removes the rows with the above Indicators considering we don't need them anyway
+patterns <- c(
+"Net coverage in secondary education",
+"Rate of grade repetition in the official sector in elementary and middle school",
+"Number of children, girls and pregnant women with initial education within the framework of comprehensive early childhood care",
+"Percentage of children in comprehensive early education services who receive six or more visits",
+"Net coverage in basic education - Men"
+)
+regex_pattern <- paste(patterns, collapse = "|")
+row_filtered <- filtered_data %>%
+filter(!str_detect(Indicator, regex_pattern))
+View(row_filtered)
+unique(row_filtered$Indicator)  ##We are now left with 29 indicators
+
+##Checking, apparently it works.
+row_duplicates <- row_filtered|>
+dplyr::summarise(n = dplyr::n(),
+.by = c(`Department Code`, Department, `Entity Code`, Entity,
+Dimension, Subcategory, Year, `Unit of Measurement`, Indicator)) |>
+dplyr::filter(n > 1L)
+View(row_duplicates)
+unique(row_duplicates$Indicator)
+
+
+## Removing rows with indicators we do not need, we are now left with 16 indicators
+
+patterns1 <- c(
+"Gross coverage in transition",
+"Gross coverage in primary education",
+"Gross coverage in secondary education" ,
+"Gross coverage in middle school education",
+"Gross coverage in basic education" ,
+"Net coverage in transition education" ,
+"Net coverage in primary education"   ,
+"Net coverage in basic education" ,
+"Rate of immediate transition to higher education" ,
+"Percentage of preschool children who receive nutritional requirements through the PAE",
+"Percentage of children whose families participate in training processes in initial education services within the framework of comprehensive care",
+"Percentage of children in early childhood receiving early childhood education within the framework of care integral con esquema de vacunación completo para la edad",
+"Net coverage in basic education - Women"
+)
+regex_pattern1 <- paste(patterns1, collapse = "|")
+row_filtered1 <- row_filtered%>%
+filter(!str_detect(Indicator, regex_pattern1))
+View(row_filtered1)
+unique(row_filtered1$Indicator) 
+
+## Converting this to wide data format
+wide_data <- row_filtered1 %>%
+pivot_wider(names_from =`Indicator`, values_from = `Numerical Data`)
+View(wide_data)
+glimpse(wide_data)
+##Exploring the wide data
+unique(wide_data$`Coverage in higher education`)
+
+sum(is.na(wide_data$`Coverage in higher education`))
 
---
+++ Practice.R
@@ -0,0 +1,234 @@
+
+##Exploring the dataset using a video on youtube
+
+library(tidyverse)
+data()
+?starwars
+dim(starwars)
+str(starwars)
+glimpse(starwars)
+view(starwars)
+head(starwars)
+tail(starwars)
+
+attach(starwars) ##lazy and not best practice
+names(starwars)
+length(starwars) 
+class(hair_color)
+unique(hair_color)
+view(sort(table(hair_color), decreasing = TRUE))
+barplot(sort(table(hair_color), decreasing = TRUE))
+
+starwars%>%
+  select(hair_color)%>%
+  count(hair_color)%>%
+  arrange(desc(n))%>%
+  view()
+
+##both above codes are the same, the second one is tidyverse
+view(starwars[is.na(hair_color), ])
+
+##whenever you use a square bracket and a comma within in, the word before the comma will tell R to
+##select certain rows which aligns to what you say
+
+class(height)
+length(height)
+summary(height)
+boxplot(height)
+hist(height)
+
+###########################
+
+##pipe operators
+
+starwars%>%
+  select(gender,height,mass,species) %>% 
+  filter(species == "Human") %>% 
+  na.omit() %>% 
+  mutate(height = height/100) %>% 
+  mutate(BMI= mass/height^2) %>% 
+  group_by(gender) %>% 
+  summarise(Average_BMI = mean(BMI))
+
+##Cleaning the data with R
+library(tidyverse)
+view(starwars)
+glimpse(starwars)
+class(starwars$gender)
+unique(starwars$gender)
+
+starwars$gender <- as.factor(starwars$gender) ##convert to a factor variable
+class(starwars$gender) 
+levels(starwars$gender)
+starwars$gender <- factor((starwars$gender),levels = c("masculine","feminine")) #changing levels in a factor
+
+starwars %>% 
+  select(name, height, ends_with("color")) %>% 
+  filter(hair_color %in% c("blond", "brown") & height < 180)
+  
+           
+#missing data
+mean(starwars$height, na.rm = TRUE) ##take the average height na.rm - if its NA then remove 
+starwars %>% 
+  select(name,gender, hair_color, height)
+
+starwars %>% 
+  select(name,gender, hair_color, height) %>% 
+  na.omit(). ##NOT RECOMMENDED - do not know what the dataset is omitting
+
+starwars %>% 
+  select(name,gender, hair_color, height) %>% 
+  filter(complete.cases(.)) ## does the same thing as na.omit - keeps the datapoints that are complete. 
+
+starwars %>% 
+  select(name,gender, hair_color, height) %>% 
+  filter(!complete.cases(.)) ##exact opposite of the above code - shows where NA actually is
+
+starwars %>% 
+  select(name,gender, hair_color, height) %>% 
+drop_na(height)
+
+starwars %>% 
+  select(name,gender, hair_color, height) %>% 
+  filter(!complete.cases(.)) %>% 
+  mutate(hair_color = replace_na(hair_color, "none"))
+
+
+##Duplicates
+Names <- c("Peter", "John", "Andrew", "Peter")
+Age <- c(22,33,44,22)
+Friends <- data.frame(Names,Age)
+Friends %>% distinct()
+
+duplicated(Friends) ##creates a logical vector
+Friends[!duplicated(Friends),]
+
+
+#Recoding variables
+starwars %>% 
+  select(name, gender) %>% 
+  mutate = (gender = recode(gender, "masculine" = 1, "feminine" = 2))
+view(starwars)
+
+## Manipulate your data. Data wrangling. R programmning for beginners.
+
+library(tidyverse)
+?msleep  
+glimpse(msleep)  
+view(msleep)  
+
+#rename a variable 
+
+msleep %>% 
+  rename("conserv" = "conservation") %>% 
+  glimpse()
+
+
+#reorder the variables
+msleep %>% 
+  select(vore, name, everything())
+
+#change variable type character to factor and then back 
+class(msleep$vore) #vore is a character
+msleep$vore <- as.factor(msleep$vore) #changes to factor 
+glimpse(msleep)
+
+#using tidyverse
+msleep %>% 
+  mutate(vore = as.character(vore)) %>% #changing back into character 
+  glimpse()
+
+#select variables to work with 
+names(msleep)
+
+msleep %>% 
+  select(2:4,
+         awake,
+         starts_with("sleep"),
+         contains("wt")) %>% 
+  names()
+
+#filter and arrange the data 
+unique(msleep$order)
+ 
+##subset a data, select rows that meet certain criterias
+
+## == is usually a question and not an assignment , | is "or"
+## The code below filters the dataset by rows, it tells R to filter on the basis of the column order 
+## and find either Carnivora or Primates who have a sleep total of more than 8
+## It furthermore selects only 3 columns for view 
+## And arranges in the decending order of the sleep
+
+msleep %>% 
+  filter((order == "Carnivora" |
+         order == "Primates") & 
+         sleep_total > 8) %>% 
+  select(name, order, sleep_total) %>% 
+  arrange(-sleep_total) %>% 
+view
+
+
+## same thing with a different code, arranged by order and written in an easier manner
+
+msleep %>% 
+  filter(order %in% c("Carnivora", "Primates") &
+        sleep_total > 8 ) %>% 
+  select(name, order, sleep_total) %>% 
+  arrange(order) %>% 
+  view
+
+
+# change observations (mutate)
+msleep %>% 
+  mutate(brainwt = brainwt * 1000) %>% 
+  view
+
+# change observations (mutate)
+msleep %>% 
+  mutate(brainwt_ingm = brainwt * 1000) %>% 
+  view
+
+
+# conditonal changes (if else)
+# logical vectors based on a condition
+
+## where is this variable greater than 0.01? This gives us a logical vector TRUE FALSE
+## if else starts with a logical fector, if the answer is true stick LARGE if its false stick FALSE
+msleep$brainwt > 0.01
+
+size_of_brain <- msleep %>% 
+  select(name, brainwt) %>% 
+  drop_na(brainwt) %>% 
+  mutate(brain_size = if_else(brainwt > 0.01, "large","small"))
+size_of_brain
+
+## Recoding data, rename a variable
+size_of_brain %>% 
+  mutate(brain_size = recode(brain_size, "large" = 1, "small" = 2))
+
+
+## long to wide OR wide to long 
+library(gapminder)
+view(gapminder)
+
+data <- select(gapminder, country, year, lifeExp)
+view(data)
+
+wide_data <- data %>% 
+  pivot_wider(names_from = year, values_from = lifeExp)
+view(wide_data)
+
+long_data <- wide_data %>% 
+  pivot_longer(2:13,
+               names_to = "year",
+               values_to = "lifeExp")
+view(long_data)
+
+
+
+
+
+
+
+
+
